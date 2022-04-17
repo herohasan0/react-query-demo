f23c17af780c6b89fba5a7640c7862e0ce6d3392
Hasan KAHRAMAN <kahraman.h@outlook.com>
2022-04-17 19:31:22 +0300

    add react query for handling server state

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import { QueryClientProvider, QueryClient } from "react-query";
+
 import Home from "./pages/Home";
 import SuperHeroes from "./pages/SuperHeroes";
 import RQHeroes from "./pages/RQHeroes";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/superheroes">SuperHeroes</Link>
-            </li>
-            <li>
-              <Link to="/rqheroes">RQHeroes</Link>
-            </li>
-          </ul>
-        </nav>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/superheroes">SuperHeroes</Link>
+              </li>
+              <li>
+                <Link to="/rqheroes">RQHeroes</Link>
+              </li>
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
+          {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/rqheroes">
-            <RQHeroes />
-          </Route>
-          <Route path="/superheroes">
-            <SuperHeroes />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route path="/rqheroes">
+              <RQHeroes />
+            </Route>
+            <Route path="/superheroes">
+              <SuperHeroes />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/RQHeroes.jsx
+++ src/pages/RQHeroes.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 export default function RQHeroes() {
-  return <div>RQHeroes</div>;
+  const { isLoading, data } = useQuery("super-heroes", () => {
+    return axios.get("http://localhost:4000/superheroes");
+  });
+
+  if (isLoading) {
+    return <div>LOADING</div>;
+  }
+  return (
+    <>
+      <h2>Super Heroes</h2>
+      {data?.data.map((hero) => (
+        <div>{hero.name}</div>
+      ))}
+    </>
+  );
 }
