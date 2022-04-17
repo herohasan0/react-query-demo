8b3997d301e3ab6f967c991f7b243a336607ed48
Hasan KAHRAMAN <kahraman.h@outlook.com>
2022-04-17 19:15:04 +0300

    get data from json server with axios

--- src/pages/SuperHeroes.jsx
+++ src/pages/SuperHeroes.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function SuperHeroes() {
-  return <div>SuperHeroes</div>;
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/superheroes").then((res) => {
+      setData(res.data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <div>LOADING</div>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes</h2>
+      {data.map((hero) => (
+        <div>{hero.name}</div>
+      ))}
+    </>
+  );
 }
