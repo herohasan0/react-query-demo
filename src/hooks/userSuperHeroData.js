6433d04eaed8e16adb439eb61d6014e60d4032db
Hasan KAHRAMAN <kahraman.h@outlook.com>
2022-04-22 00:16:07 +0300

    add initial query data

--- src/hooks/userSuperHeroData.js
+++ src/hooks/userSuperHeroData.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 const fetchSuperHero = (heroId) => {
   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
@@ -17,5 +17,25 @@ const fetchSuperHero2 = ({ queryKey }) => {
 ///// OR
 
 export const useSuperHeroData = (heroId) => {
-  return useQuery(["super-hero", heroId], fetchSuperHero2);
+  console.log("heroid", heroId);
+  const queryClient = useQueryClient();
+  console.log("queryClient", queryClient);
+
+  return useQuery(["super-hero", heroId], fetchSuperHero2, {
+    initialData: () => {
+      console.log("heroid***2", heroId);
+
+      const hero = queryClient
+        .getQueryData("super-heroes")
+        ?.data?.find((hero) => hero.id === parseInt(heroId));
+      console.log("we already have", hero);
+      if (hero) {
+        return {
+          data: hero,
+        };
+      } else {
+        return undefined;
+      }
+    },
+  });
 };
